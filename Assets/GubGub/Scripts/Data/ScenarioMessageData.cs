9a299d875b03e0ffa107eb933e710278d2f5a70a
downey <downeylefou@yahoo.co.jp>
2019-04-30 11:51:28 +0900

    メッセージ表示用パラメータクラスを追加

---
+++ Assets/GubGub/Scripts/Data/ScenarioMessageData.cs
@@ -0,0 +1,29 @@
+﻿namespace GubGub.Scripts.Data
+{
+    /// <summary>
+    /// メッセージ表示用パラメータクラス
+    /// </summary>
+    public class ScenarioMessageData
+    {
+        public string message;
+        public string speakerName;
+        public int messageSpeed;
+        public bool isSkip;
+
+
+        /// <summary>
+        /// パラメータを設定する
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="speakerName"></param>
+        /// <param name="messageSpeed"></param>
+        /// <param name="isSkip"></param>
+        public void SetParam(string message, string speakerName, int messageSpeed, bool isSkip)
+        {
+            this.message = message;
+            this.speakerName = speakerName;
+            this.messageSpeed = messageSpeed;
+            this.isSkip = isSkip;
+        }
+    }
+}
