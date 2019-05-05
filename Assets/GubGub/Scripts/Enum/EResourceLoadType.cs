3123a877c8379a6156c9656031052d3e44a37353
downey <downeylefou@yahoo.co.jp>
2019-05-05 18:00:11 +0900

    リソース読み込み先定数クラスを追加

---
+++ Assets/GubGub/Scripts/Enum/EResourceLoadType.cs
@@ -0,0 +1,12 @@
+﻿namespace GubGub.Scripts.Enum
+{
+    /// <summary>
+    /// リソースの読み込み先のタイプ
+    /// </summary>
+    public enum EResourceLoadType
+    {
+        Resources,
+        StreamingAssets,
+        Server
+    }
+}
