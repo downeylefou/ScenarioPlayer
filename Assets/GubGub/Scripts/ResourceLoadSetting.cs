a42a99ad6084733010d8b37a26f77a61f4c9ec6b
downey <downeylefou@yahoo.co.jp>
2019-05-05 17:59:37 +0900

    リソース設定クラスのリネーム

--- Assets/GubGub/Scripts/ResourceLoadSetting.cs
+++ Assets/GubGub/Scripts/ResourceLoadSetting.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using GubGub.Scripts.Enum;
 
 namespace GubGub.Scripts
 {
     /// <summary>
     /// リソースのパスを解決するクラス
     /// </summary>
-    public static class ResourcePathSetting
+    public static class ResourceLoadSetting
     {
         public static string BackgroundResourcePrefix = "texture/background/";
         public static string StandResourcePrefix = "texture/character/";
         public static string BgmResourcePrefix = "sound/bgm/";
         public static string SeResourcePrefix = "sound/se/";
         public static string ScenarioResourcePrefix = "scenario/";
+        
+        /// <summary>
+        /// リソースの読み込み先
+        /// </summary>
+        public static EResourceLoadType ResourceLoadType { get; set; }
     }
 }
