1c9041d5a573b8b77fb803c7ce96e5530cf0f8c2
downey <downeylefou@yahoo.co.jp>
2019-06-02 16:46:40 +0900

    ジャンプコマンド、ラベルコマンドクラスを追加

---
+++ Assets/GubGub/Scripts/Command/JumpCommand.cs
@@ -0,0 +1,30 @@
+﻿using GubGub.Scripts.Enum;
+
+namespace GubGub.Scripts.Command
+{
+    /// <summary>
+    /// 特定のラベルに飛ぶためのパラメータを持つ
+    /// </summary>
+    public class JumpCommand : BaseScenarioCommand
+    {
+        public string LabelName { get; private set; }
+
+
+        public JumpCommand() : base(EScenarioCommandType.Jump)
+        {
+        }
+
+        protected override void Reset()
+        {
+            LabelName = "";
+        }
+
+        protected sealed override void MapParameters()
+        {
+            if (rawParams.Count > 0)
+            {
+                LabelName = GetString(0, null);
+            }
+        }
+    }
+}
---
+++ Assets/GubGub/Scripts/Command/LabelCommand.cs
@@ -0,0 +1,30 @@
+﻿using GubGub.Scripts.Enum;
+
+namespace GubGub.Scripts.Command
+{
+    /// <summary>
+    /// 特定のラベルを表すためのパラメータを持つ
+    /// </summary>
+    public class LabelCommand : BaseScenarioCommand
+    {
+        public string LabelName { get; private set; }
+
+
+        public LabelCommand() : base(EScenarioCommandType.Label)
+        {
+        }
+
+        protected override void Reset()
+        {
+            LabelName = "";
+        }
+
+        protected sealed override void MapParameters()
+        {
+            if (rawParams.Count > 0)
+            {
+                LabelName = GetString(0, null);
+            }
+        }
+    }
+}
