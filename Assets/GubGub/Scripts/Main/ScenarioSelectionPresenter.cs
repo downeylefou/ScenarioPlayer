87e439df941f56b65dc284feaa0d560297ca3132
downey <downeylefou@yahoo.co.jp>
2019-06-09 12:27:20 +0900

    選択肢表示用クラスとプレハブを追加

---
+++ Assets/GubGub/Scripts/Main/ScenarioSelectionPresenter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using GubGub.Scripts.Command;
+using GubGub.Scripts.View;
+using UniRx;
+using UnityEngine;
+
+namespace GubGub.Scripts.Main
+{
+    /// <summary>
+    /// シナリオの選択肢のプレゼンタ
+    /// </summary>
+    public class ScenarioSelectionPresenter : MonoBehaviour
+    {
+        /// <summary>
+        /// 表示する選択肢のもととなるプレハブ
+        /// </summary>
+        [SerializeField] private GameObject selectionPrefab;
+
+        /// <summary>
+        /// 選択肢がクリックされたことを通知する
+        /// </summary>
+        public Subject<string> onSelect = new Subject<string>();
+        
+        /// <summary>
+        /// 選択肢ビューのリスト
+        /// </summary>
+        private readonly List<ScenarioSelectionView> _viewList = new List<ScenarioSelectionView>();
+        
+        
+        public void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// 表示を初期化する
+        /// </summary>
+        public void Clear()
+        {
+            _viewList.ForEach(Destroy);
+            _viewList.Clear();
+        }
+                
+        /// <summary>
+        /// 選択肢ビューを追加する
+        /// </summary>
+        /// <param name="command"></param>
+        public void AddSelection(SelectionCommand command)
+        {
+            var view = Instantiate(selectionPrefab, transform)
+                .GetComponent<ScenarioSelectionView>();
+            
+            _viewList.Add(view);
+            view.Initialize(command.SelectionText, command.LabelName, OnClick);
+            
+            void OnClick()
+            {
+                onSelect.OnNext(command.LabelName);
+            }
+        }
+        
+    }
+}
---
+++ Assets/GubGub/Scripts/View/ScenarioSelectionView.cs
@@ -0,0 +1,55 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace GubGub.Scripts.View
+{
+    /// <summary>
+    /// シナリオの選択肢のビュー
+    /// </summary>
+    public class ScenarioSelectionView : MonoBehaviour
+    {
+        [SerializeField] private Text selectionMessage;
+        
+        [SerializeField] private Button button;
+
+        /// <summary>
+        /// クリックイベント
+        /// </summary>
+        private UnityAction _onClick;
+
+        /// <summary>
+        /// 選択した際の遷移先ラベル名
+        /// </summary>
+        private string _labelName;
+
+        
+        private void Awake()
+        {
+            button.onClick.AsObservable().Subscribe(_ => OnClickButton());
+        }
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        /// <param name="messageText"></param>
+        /// <param name="labelName"></param>
+        /// <param name="onClick"></param>
+        public void Initialize(string messageText, string labelName, UnityAction onClick)
+        {
+            selectionMessage.text = messageText;
+            _labelName = labelName;
+            _onClick = onClick;
+        }
+        
+        /// <summary>
+        /// ボタンのクリック時
+        /// コールバックがあれば実行する
+        /// </summary>
+        private void OnClickButton()
+        {
+            _onClick?.Invoke();
+        }
+    }
+}
