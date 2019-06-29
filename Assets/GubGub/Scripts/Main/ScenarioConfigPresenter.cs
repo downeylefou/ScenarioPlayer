4b411d2d712cf6f6f4bd0c17f13ae16a6892fbd5
downey <downeylefou@yahoo.co.jp>
2019-06-29 16:33:49 +0900

    シナリオコンフィグクラスを追加

---
+++ Assets/GubGub/Scripts/Main/ScenarioConfigPresenter.cs
@@ -0,0 +1,71 @@
+﻿using GubGub.Scripts.View;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace GubGub.Scripts.Main
+{
+    /// <summary>
+    /// シナリオコンフィグビューの管理クラス
+    /// </summary>
+    public class ScenarioConfigPresenter : MonoBehaviour
+    {
+        [SerializeField] private ScenarioConfigView view;
+
+        [SerializeField] private Button dimmer;
+        
+        /// <summary>
+        /// Bgmのボリューム値が変更されたことを通知する
+        /// </summary>
+        public Subject<float> changedBgmVolume = new Subject<float>();
+        
+        /// <summary>
+        /// Seのボリューム値が変更されたことを通知する
+        /// </summary>
+        public Subject<float> changedSeVolume = new Subject<float>();
+        
+        /// <summary>
+        /// 暗幕のボタンイベント
+        /// </summary>
+        public UnityAction onTouchDimmer;
+
+        
+        private void Awake()
+        {
+            AddEventListener();
+            Bind();
+            
+            Hide();
+        }
+        
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+        
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+        
+        private void AddEventListener()
+        {
+            dimmer.onClick.AddListener(OnClose);
+            view.onCloseButton = OnClose;
+        }
+
+        private void Bind()
+        {
+            view.changedBgmVolume.Subscribe(changedBgmVolume).AddTo(this);
+            view.changedSeVolume.Subscribe(changedSeVolume).AddTo(this);
+        }
+
+        private void OnClose()
+        {
+            onTouchDimmer?.Invoke();
+        }
+    }
+
+
+}
---
+++ Assets/GubGub/Scripts/View/ScenarioConfigView.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace GubGub.Scripts.View
+{
+    /// <summary>
+    /// シナリオコンフィグのビュークラス
+    /// </summary>
+    public class ScenarioConfigView : MonoBehaviour
+    {
+        [SerializeField] private Button closeButton;
+
+        [SerializeField] private Slider bgmVolumeSlider;
+
+        [SerializeField] private Slider seVolumeSlider;
+
+        /// <summary>
+        /// 閉じるボタンのイベント
+        /// </summary>
+        public UnityAction onCloseButton; 
+        
+        /// <summary>
+        /// Bgmのボリューム値が変更されたことを通知する
+        /// </summary>
+        public FloatReactiveProperty changedBgmVolume = new FloatReactiveProperty();
+  
+        /// <summary>
+        /// Seのボリューム値が変更されたことを通知する
+        /// </summary>
+        public FloatReactiveProperty changedSeVolume = new FloatReactiveProperty();
+        
+        
+        private void Awake()
+        {
+            AddEventListener();
+            Bind();
+        }
+        
+        private void AddEventListener()
+        {
+            closeButton.onClick.AddListener(onCloseButton);
+        }
+
+        private void Bind()
+        {
+            bgmVolumeSlider.OnValueChangedAsObservable()
+                .Subscribe(_ => changedBgmVolume.Value = _).AddTo(this);
+            
+            seVolumeSlider.OnValueChangedAsObservable()
+                .Subscribe(_ => changedSeVolume.Value = _).AddTo(this);
+        }
+        
+
+    }
+    
+}
