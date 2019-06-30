e2c1364ff9224d3bdfd9c1c09b95be274d9a3d6c
downey <downeylefou@yahoo.co.jp>
2019-06-30 15:15:18 +0900

    PlayerDataManager　プレイヤーデータの読み込み・保存クラスを追加

---
+++ Assets/GubGub/Scripts/Lib/PlayerDataManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace GubGub.Scripts.Lib
+{
+    /// <summary>
+    /// プレイヤーデータの管理クラス
+    /// </summary>
+    public static class PlayerDataManager
+    {
+        /// <summary>
+        /// Floatのパラメータをロードする
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static float LoadFloat(string key, float defaultValue)
+        {
+            return PlayerPrefs.GetFloat(key, defaultValue);
+        }
+        
+        /// <summary>
+        /// Floatのパラメータをセーブする
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static void SetFloat(string key, float value)
+        {
+            PlayerPrefs.SetFloat(key, value);
+        }
+        
+        /// <summary>
+        /// プレイヤーデータを保存する
+        /// </summary>
+        public static void Save()
+        {
+            PlayerPrefs.Save();
+        }
+    }
+}
