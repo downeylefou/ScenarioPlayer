fc3d0d399451b68fc2c0ca7f472db9769a4c69b5
downey <downeylefou@yahoo.co.jp>
2019-06-30 15:15:42 +0900

    コンフィグパラメータの定数クラスを追加

---
+++ Assets/GubGub/Scripts/Enum/EScenarioConfigKey.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GubGub.Scripts.Enum
+{
+
+    /// <summary>
+    /// シナリオ設定データの定数
+    /// </summary>
+    public enum EScenarioConfigKey
+    {
+        BgmVolume,
+        SeVolume,
+    }
+
+    /// <summary>
+    ///  EScenarioConfigKeyの拡張クラス
+    /// </summary>
+    internal static class EScenarioConfigKeyExtension
+    {
+        public static Dictionary<EScenarioConfigKey, string> NameList { get;} =
+            new Dictionary<EScenarioConfigKey, string>()
+            {
+                {EScenarioConfigKey.BgmVolume, "bgm_volume"},
+                {EScenarioConfigKey.SeVolume, "se_volume"},
+            };
+        
+        /// <summary>
+        ///  Enumに対応した文字列を取得する
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetName(this EScenarioConfigKey value)
+        {
+            return NameList[value];
+        }
+        
+    }
+}
