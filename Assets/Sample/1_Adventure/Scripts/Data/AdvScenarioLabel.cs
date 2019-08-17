a673bcdfc4c5b7232825ffec9a0a0d386d54b158
downey <downeylefou@yahoo.co.jp>
2019-08-17 13:40:43 +0900

    サンプル　シナリオラベルクラスの追加

---
+++ Assets/Sample/1_Adventure/Scripts/Data/AdvScenarioLabel.cs
@@ -0,0 +1,25 @@
+﻿namespace Sample._1_Adventure.Scripts.Data
+{
+    /// <summary>
+    /// シナリオ再生の起点となるラベル名
+    /// </summary>
+    public enum AdvScenarioLabel
+    {
+    }
+
+    /// <summary>
+    /// AdvScenarioLabelの拡張クラス
+    /// </summary>
+    public static class AdvScenarioLabelExtension
+    {
+        /// <summary>
+        /// ラベル名を文字列で取得する
+        /// </summary>
+        /// <param name="label"></param>
+        /// <returns></returns>
+        public static string GetName(this AdvScenarioLabel label)
+        {
+            return System.Enum.GetName(typeof(AdvScenarioLabel), label);
+        }
+    }
+}
