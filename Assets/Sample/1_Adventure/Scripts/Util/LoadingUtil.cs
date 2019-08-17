c4af0fc30314db9cfa00496b6837830d41e30b1e
downey <downeylefou@yahoo.co.jp>
2019-08-17 13:34:02 +0900

    サンプル　ローディングビューを追加

---
+++ Assets/Sample/1_Adventure/Scripts/Util/LoadingUtil.cs
@@ -0,0 +1,41 @@
+﻿using Sample._1_Adventure.Scripts.View;
+using UniRx.Async;
+
+namespace Sample._1_Adventure.Scripts.Util
+{
+    /// <summary>
+    /// ローディングのユーティリティクラス
+    /// </summary>
+    public static class LoadingUtil
+    {
+        private static LoadingView _view;
+
+        public static void Initialize(LoadingView view)
+        {
+            _view = view;
+        }
+
+        /// <summary>
+        /// ローディングを表示する
+        /// </summary>
+        /// <returns></returns>
+        public static async UniTask Show()
+        {
+            await _view.Show();
+        }
+
+        /// <summary>
+        /// ローディングを非表示にする
+        /// </summary>
+        /// <returns></returns>
+        public static async UniTask Hide()
+        {
+            await _view.Hide();
+        }
+        
+        public static async UniTask Wait(float duration = 1f)
+        {
+            await _view.Wait(duration);
+        }
+    }
+}
---
+++ Assets/Sample/1_Adventure/Scripts/View/LoadingView.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using GubGub.Scripts.Lib;
+using UniRx.Async;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sample._1_Adventure.Scripts.View
+{
+    /// <summary>
+    /// ローディングなどで画面を覆うビュー
+    /// </summary>
+    public class LoadingView : MonoBehaviour
+    {
+        [SerializeField] private Image left;
+        [SerializeField] private Image right;
+
+        private const float Duration = 1.0f;
+
+
+        /// <summary>
+        /// ローディングを表示する
+        /// </summary>
+        public async UniTask Show()
+        {
+            var sequence = DOTween.Sequence();
+            await sequence
+                .Append(left.rectTransform.DOLocalMoveX(0, Duration))
+                .Join(right.rectTransform.DOLocalMoveX(0, Duration))
+                .Play();
+        }
+
+        /// <summary>
+        /// ローディングを非表示にする
+        /// </summary>
+        public async UniTask Hide()
+        {
+            var moveValue = left.rectTransform.sizeDelta.x;
+
+            var sequence = DOTween.Sequence();
+            await sequence
+                .Append(left.rectTransform.DOLocalMoveX(-moveValue, Duration))
+                .Join(right.rectTransform.DOLocalMoveX(moveValue, Duration))
+                .Play();
+        }
+
+        public async UniTask Wait(float duration = 1f)
+        {
+            await DOVirtual.DelayedCall(duration, () => { }).Play();
+        }
+    }
+}
