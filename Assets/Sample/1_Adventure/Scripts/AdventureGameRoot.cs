4c20eb3bd780e16ba265cb4ebd981dcd1d43c714
downey <downeylefou@yahoo.co.jp>
2019-08-17 13:33:17 +0900

    サンプル　シーン、Rootクラスの追加

--- Assets/Sample/1_Adventure/Scripts/AdventureGameRoot.cs
+++ Assets/Sample/1_Adventure/Scripts/AdventureGameRoot.cs
@@ -1,5 +1,9 @@
-﻿using Sample._0_Test.Scripts;
+﻿using GubGub.Scripts.Main;
+using Sample._0_Test.Scripts;
+using Sample._1_Adventure.Scripts.Util;
+using Sample._1_Adventure.Scripts.View;
 using UniRx;
+using UniRx.Async;
 using UnityEngine;
 
 namespace Sample._1_Adventure.Scripts
@@ -9,26 +13,47 @@ namespace Sample._1_Adventure.Scripts
     /// </summary>
     public class AdventureGameRoot : MonoBehaviour
     {
+        [SerializeField] private ScenarioStarter scenarioStarter;
+
         [SerializeField] private TitleScene titleScene;
         [SerializeField] private GameScene gameScene;
 
-        
-        private void Start()
+        private async void Start()
+        {
+            // ローディングの初期化
+            LoadingUtil.Initialize(FindObjectOfType<LoadingView>());
+            
+            // シナリオプレイヤーの初期化
+            await scenarioStarter.Initialize();
+            AdvScenarioUtil.Initialize(scenarioStarter);
+            AdvScenarioUtil.Hide();
+
+            await AdvScenarioUtil.LoadScenario("advTest");
+
+            Bind();
+        }
+
+        private void Bind()
         {
             // タイトルシーンでボタンが押されたらゲームシーンに移動する
-            titleScene.OnClickStartButton.
-                Subscribe(_ => MoveGameScene()).AddTo(this);
+            titleScene.OnClickStartButton.Subscribe(
+                async _ => await MoveGameScene()).AddTo(this);
         }
 
         /// <summary>
         /// ゲームシーンに移動する
         /// </summary>
-        private void MoveGameScene()
+        private async UniTask MoveGameScene()
         {
+            await LoadingUtil.Show();
+            
             gameScene.gameObject.SetActive(true);
             titleScene.gameObject.SetActive(false);
+
+            await LoadingUtil.Wait();
+            await LoadingUtil.Hide();
+
+            gameScene.StartScene();
         }
     }
-    
-
 }
