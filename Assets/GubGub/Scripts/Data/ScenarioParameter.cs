c7d2576fb597073e6612873b834c2355c479607b
downey <downeylefou@yahoo.co.jp>
2019-08-25 15:21:14 +0900

    パラメータクラスを追加

---
+++ Assets/GubGub/Scripts/Data/ScenarioParameter.cs
@@ -0,0 +1,20 @@
+﻿namespace GubGub.Scripts.Data
+{
+    /// <summary>
+    /// シナリオで使用するパラメータクラス
+    /// </summary>
+    public class ScenarioParameter
+    {
+        private object _value;
+
+        public T GetParameter<T>()
+        {
+            return (T) _value;
+        }
+
+        public void SetValue<T>(T value)
+        {
+            _value = value;
+        }
+    }
+}
---
+++ Assets/GubGub/Scripts/Enum/EScenarioParameterType.cs
@@ -0,0 +1,13 @@
+﻿namespace GubGub.Scripts.Enum
+{
+    /// <summary>
+    /// シナリオパラメータのタイプ
+    /// </summary>
+    public enum EScenarioParameterType
+    {
+        None,
+        Float,
+        String,
+        Bool,
+    }
+}
