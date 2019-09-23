99ceee266108561d0255515b1d60733a2e9db46d
downey <downeylefou@yahoo.co.jp>
2019-09-23 16:49:07 +0900

    設定シートの値でコマンドのパラメータを更新するクラスを追加

---
+++ Assets/GubGub/Scripts/Main/CommandUpdater.cs
@@ -0,0 +1,37 @@
+﻿using GubGub.Scripts.Command;
+using GubGub.Scripts.Enum;
+using GubGub.Scripts.Lib.ResourceSetting;
+
+namespace GubGub.Scripts.Main
+{
+    /// <summary>
+    /// コマンドのパラメータを設定シートの値で更新する
+    /// </summary>
+    public class CommandUpdater
+    {
+        /// <summary>
+        /// コマンドのパラメータを設定シートのEntityで更新する
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="settingModel"></param>
+        public void UpdateCommand(BaseScenarioCommand command, ScenarioResourceSettingModel settingModel)
+        {
+            if (command is StandCommand standCommand)
+            {
+                UpdateStandCommand(standCommand, settingModel);
+            }
+            else if (command is BgmCommand)
+            {
+            }
+        }
+
+        private void UpdateStandCommand(StandCommand command, ScenarioResourceSettingModel settingModel)
+        {
+            if (settingModel.GetSettingEntity(command.StandName,
+                EResourceType.Character) is CharacterSheetEntity entity)
+            {
+                command.UpdateParameter(entity);
+            }
+        }
+    }
+}
