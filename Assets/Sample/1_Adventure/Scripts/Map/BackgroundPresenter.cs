a723f977fb89298d0fc1b3381fcb46d92ee34b37
downey <downeylefou@yahoo.co.jp>
2020-04-11 12:28:13 +0900

    サンプル　背景のプレゼンタを追加

---
+++ Assets/Sample/1_Adventure/Scripts/Map/BackgroundPresenter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sample._1_Adventure.Scripts.Map
+{
+    /// <summary>
+    /// ゲームシーンの背景を表示する
+    /// </summary>
+    public class BackgroundPresenter : MonoBehaviour
+    {
+        [SerializeField] private Image map1;
+        [SerializeField] private Image map2;
+
+        public void ChangeBackground(int mapNumber)
+        {
+            map1.gameObject.SetActive(mapNumber == 1);
+            map2.gameObject.SetActive(mapNumber == 2);
+        }
+    }
+}
