d4d0c69a8c5747f2b3e0fa6af530ed36d2ecea8b
downey <downeylefou@yahoo.co.jp>
2020-04-11 12:27:50 +0900

    サンプル　シナリオで使用するパラメータクラスを追加

---
+++ Assets/Sample/1_Adventure/Scripts/Data/AdvParameter.cs
@@ -0,0 +1,22 @@
+﻿namespace Sample._1_Adventure.Scripts.Data
+{
+    /// <summary>
+    /// ゲームで使用するパラメータ
+    /// </summary>
+    public static class AdvParameter
+    {
+        /// <summary>
+        /// 現在のマップ番号
+        /// <para>デフォルトは1だが、任意のマップからテストしたい時は変更する</para>
+        /// </summary>
+        public static int CurrentMapNumber = 1;
+
+        // マップ1用のパラメータ
+        public static bool _1_isEndStartLabel;
+        public static bool _1_isGetMashRoom;
+        
+        // マップ2用のパラメータ
+        public static bool _2_isGetAcorn1;
+        public static bool _2_isGetAcorn2;
+    }
+}
---
+++ Assets/Sample/1_Adventure/Scripts/Data/AdvScenarioParameter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sample._1_Adventure.Scripts.Data
+{
+    /// <summary>
+    /// シナリオで使用するパラメータ
+    /// </summary>
+    public enum AdvScenarioParameter
+    {
+        MashRoomCount,
+        AcornCount,
+    }
+
+    /// <summary>
+    /// AdvScenarioParameterの拡張クラス
+    /// </summary>
+    public static class AdvScenarioParameterExtension
+    {
+        /// <summary>
+        /// ラベル名を文字列で取得する
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public static string GetName(this AdvScenarioParameter parameter)
+        {
+            return Enum.GetName(typeof(AdvScenarioParameter), parameter);
+        }
+    }
+}
